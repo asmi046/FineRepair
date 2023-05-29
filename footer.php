c6aaabd34dce6fc82fa778bbf036c8927875555c
asmi046 <asmi046@gmail.com>
2023-05-29 21:03:27 +0300

    Final

--- footer.php
+++ footer.php
@@ -13,54 +13,24 @@
 
 ?>
 <footer class="footer">
-    <div class="footer__wrap wrap flex flex--aic flex--jcsb">
-        <div class="footer__logotype">
-            <a href="/" class="footerLogotype flex flex--aic flex--jcs">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/logotype-white.png" alt="" class="footerLogotype__image">
-            </a>
-        </div>
-        <div class="footer__nav">
-            <!-- <div class="footerNav flex flex--aic flex--jcs">
-                <div class="footerNav__item">
-                    <a href="/prices/" class="footerNav__link ">Цены</a>
-                </div>
-                <div class="footerNav__item">
-                    <a href="/sales/" class="footerNav__link ">Акции</a>
-                </div>
-                <div class="footerNav__item">
-                    <a href="/about/" class="footerNav__link ">О компании</a>
-                </div>
 
-                <div class="footerNav__item">
-                    <a href="/reviews/" class="footerNav__link ">Отзывы</a>
-                </div>
-                <div class="footerNav__item">
-                    <a href="/contacts/" class="footerNav__link ">Контакты</a>
-                </div>
-            </div> -->
-        </div>
-        
-        <!-- <div class="footer__covid">
-            <div class="footerCovid">
-                <div class="footerCovid__icon"></div>
-                <div class="footerCovid__text">
-                    <div class="footerCovid__close"><svg height="311pt" viewBox="0 0 311 311.07733" width="311pt" xmlns="http://www.w3.org/2000/svg"><path d="m16.035156 311.078125c-4.097656 0-8.195312-1.558594-11.308594-4.695313-6.25-6.25-6.25-16.382812 0-22.632812l279.0625-279.0625c6.25-6.25 16.382813-6.25 22.632813 0s6.25 16.382812 0 22.636719l-279.058594 279.058593c-3.136719 3.117188-7.234375 4.695313-11.328125 4.695313zm0 0"/><path d="m295.117188 311.078125c-4.097657 0-8.191407-1.558594-11.308594-4.695313l-279.082032-279.058593c-6.25-6.253907-6.25-16.386719 0-22.636719s16.382813-6.25 22.636719 0l279.058594 279.0625c6.25 6.25 6.25 16.382812 0 22.632812-3.136719 3.117188-7.230469 4.695313-11.304687 4.695313zm0 0"/></svg></div>
-                    <p>Мы работаем в обычном режиме. Мастера выезжают на дом без выходных и праздников.</p>
-                    <p><b>Каждый день</b> мы проверяем у мастеров температуру и следим за их самочувствием.</p>
-                    <p>Наша задача сделать так, чтобы <b>вы были спокойны</b> не только за свою технику, но и <b>за свое здоровье</b>.</p>
-                </div>
-            </div>
-        </div> -->
-
-        <div class="footer__phone">
-            <div class="footerPhone">
-                <a href="tel:<?php the_field('telefon', 'options'); ?>" class="footerPhone__link"><?php the_field('telefon', 'options'); ?></a>
-            </div>
-        </div>
-    </div>
 
     <div class="footer__wrap footer__wrap__dop wrap flex flex--aic flex--jcsb">
         <div class="foot_3_col">
+            <div>
+                <div class="footer__logotype">
+                    <a href="/" class="footerLogotype flex flex--aic flex--jcs">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/logotype-white.png" alt="" class="footerLogotype__image">
+                    </a>
+                </div>
+                <p class="subtext_footer">Качественный и срочный ремонт бытовой техники в Москве и Московской области</p>
+
+                <figure class="pm">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/moscow-white.png" data-src="https://computer-master.org/themes/starterkit/assets/resources/images/moscow-white.png" alt="Логотип компании" title="Логотип компании" data-uk-img="">
+                    <span>При поддержке <br/>Правительства <br/>Москвы</span>
+                </figure>
+            </div>
+
             <div>
                 <h3>Клиентам</h3>
                 <a href="/prices/">Цены</a><br>
@@ -82,8 +52,14 @@
             
 
             
-            <div >
-                <span class="time_work">Работаем без выходных с 9:00 до 22:00</span><br/><br/>
+            <div>
+                <div class="footer__phone">
+                    <div class="footerPhone">
+                        <a href="tel:<?php the_field('telefon', 'options'); ?>" class="footerPhone__link"><?php the_field('telefon', 'options'); ?></a>
+                    </div>
+                </div>
+
+                <span class="time_work">Работаем без выходных с 9:00 до 22:00</span><br/>
                 <span class="map_pin">Ходынская улица, 3с4</span>
             </div>
         </div>
